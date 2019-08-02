88b97d1d621b303c5588414259a0947316a72fae
qingjiang.li@ninebot.com <qing_jiang_li@163.com>
2019-08-02 20:47:41 +0800

    add error logger to weixin qun by logback config

---
+++ uzhizhe-ninebot-web/src/test/java/com/uzhizhe/ninebot/weixin/WeixinMessageTest.java
@@ -0,0 +1,43 @@
+package com.uzhizhe.ninebot.weixin;
+
+import com.monker.common.weixin.WeixinMessageSender;
+import com.monker.common.weixin.domain.TextContext;
+import com.uzhizhe.ninebot.TestApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @Desc
+ * @Author qingjiang.li
+ * @Email qingjiang.li@ninebot.com
+ * @Date 2019-08-02
+ */
+@Slf4j
+public class WeixinMessageTest extends TestApplication {
+    @Test
+    public void test13() {
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9a408d7f-23c3-4bbf-bd45-0f7291da9744";
+        TextContext textContext = new TextContext();
+        textContext.setContent("test message");
+        textContext.setMentioned_mobile_list(Arrays.asList("18339922767"));
+        textContext.setMentioned_list(Arrays.asList("qingjiang.li", "@all"));
+        WeixinMessageSender sender = new WeixinMessageSender();
+        sender.sendMessage(url, textContext);
+
+    }
+
+    @Test
+    public void test30() throws Exception {
+
+        log.error("Test logback wx error logger");
+        log.warn("Test logback wx warn logger");
+        log.trace("Test loback wx trace logger");
+        log.info("Test info logger");
+        log.debug("Test debug logger");
+
+    }
+
+
+}
