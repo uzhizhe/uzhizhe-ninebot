c8419d892b9aebe0b30465bcbd8cc249d3cd06a7
qingjiang.li@ninebot.com <qing_jiang_li@163.com>
2019-08-05 21:00:47 +0800

    add info

---
+++ uzhizhe-ninebot-web/src/test/java/com/uzhizhe/ninebot/string/CodeTest.java
@@ -0,0 +1,25 @@
+package com.uzhizhe.ninebot.string;
+
+import org.junit.Test;
+
+/**
+ * @Desc test
+ * @Author qingjiang.li
+ * @Email qingjiang.li@ninebot.com
+ * @Date 2019-08-05
+ */
+public class CodeTest {
+
+
+    @Test
+    public void testj15() {
+        int arr[] = {6, 1, 2, 3, 4, 5};
+        Integer max = getMax(arr);
+        System.out.println(max);
+    }
+
+    public Integer getMax(int[] a) {
+
+        return null;
+    }
+}
--- uzhizhe-ninebot-web/src/test/java/com/uzhizhe/ninebot/string/StringTest.java
+++ uzhizhe-ninebot-web/src/test/java/com/uzhizhe/ninebot/string/StringTest.java
@@ -35,4 +35,59 @@ public class StringTest {
         User user = JSON.parseObject(s, User.class);
         System.out.println(user);
     }
+
+    @Test
+    public void test40() {
+        String s1 = new String("abc") + new String("abc");
+        String s1intern = s1.intern();
+        String s2 = "abcabc";
+        System.out.println(s1 == s2);//true
+        System.out.println(s1 == s1intern);//true
+        System.out.println(s2 == s1intern);//true
+
+    }
+
+
+    @Test
+    public void test55() {
+        String s1 = new String("ABC");
+        String s2 = "ABC";
+        String s3 = s1.intern();
+        String s4 = s1 + s2;
+        String s5 = new String("ABC") + new String("ABC");
+        String s6 = "ABCABC";
+        String s7 = s5.intern();
+
+        System.out.println(s1 == s2);//false
+        System.out.println(s1 == s3);//true - false
+        System.out.println(s2 == s3);//true
+        System.out.println(s4 == s5);//false
+        System.out.println(s4 == s6);//true - false
+        System.out.println(s5 == s6);//false
+        System.out.println(s6 == s7);//true
+    }
+
+
+    @Test
+    public void test71() {
+
+        Integer i1 = 110;
+        Integer i2 = new Integer(110);
+        int i3 = 110;
+
+        System.out.println(i1 == i2);
+        System.out.println(i1 == i3);
+        System.out.println(i2 == i3);
+
+        System.out.println();
+        Integer i4 = 520;
+        Integer i5 = new Integer(520);
+        int i6 = 520;
+
+        System.out.println(i4 == i5);
+        System.out.println(i4 == i6);
+        System.out.println(i5 == i6);
+
+
+    }
 }
